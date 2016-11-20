0f1d2c534848f401aa5f324e86ae8a4fdbcde1f6
Coenraad  Stijne <coenraad.stijne@gmail.com>
2016-11-20 19:49:47 +0100

    code cleanup, need to fix incremental update range

--- src/document.ts
+++ src/document.ts
@@ -30,31 +30,81 @@ export default class Document {
 
         for (let i = 0; i < textEditor.document.lineCount; i++) {
             {
-                let bracketCount: { [character: string]: number; } = {};
-                this.bracketPairs.forEach(bracketPair => {
-                    bracketCount[bracketPair.openCharacter] = 0;
-                    bracketCount[bracketPair.closeCharacter] = 0;
-                });
-
-                this.bracketsPerLine.push(bracketCount);
+                this.bracketsPerLine.push(this.getEmptyBracketArray());
             }
         }
+    }
+
+    getEmptyBracketArray() {
+        let bracketCount: { [character: string]: number; } = {};
+        for (let bracketPair of this.bracketPairs) {
+            bracketCount[bracketPair.openCharacter] = 0;
+            bracketCount[bracketPair.closeCharacter] = 0;
+        }
+
+        return bracketCount;
     }
 
     onDidChangeTextDocument(contentChanges: vscode.TextDocumentContentChangeEvent[]) {
-        let regex = new RegExp(this.regexPattern);
+        let lowestLineNumberChanged = this.textEditor.document.lineCount - 1;
+        let highestLineNumberChanged = 0;
+
         for (let contentChange of contentChanges) {
-            // TODO Implement virtual document tracking to detect deleted text
-            // TODO Per line record amount of brackets for optimization, no need to parse whole document per change
-            if (contentChange.text === "" || regex.test(contentChange.text)) {
-                this.updateDecorations(contentChange.range.start.line);
-                return;
+            if (this.updateRequired(contentChange)) {
+                lowestLineNumberChanged = Math.min(lowestLineNumberChanged, contentChange.range.start.line);
+                highestLineNumberChanged = Math.max(highestLineNumberChanged, contentChange.range.end.line);
             }
         }
+
+        this.updateDecorations(lowestLineNumberChanged);
     }
 
-    updateDecorations(lineNumber = 0) {
-        console.log("Colorizing brackets");
+    updateRequired(contentChange: vscode.TextDocumentContentChangeEvent) {
+        let regex = new RegExp(this.regexPattern);
+        let splitContent = contentChange.text.split("\r\n");
+        if (splitContent.length > 1) {
+            let amountOfNewLines = splitContent.length - 1;
+            let emptyBrackets: { [character: string]: number; }[] = [];
+
+            for (let i = 0; i <= amountOfNewLines; i++) {
+                emptyBrackets.push(this.getEmptyBracketArray());
+            }
+
+            console.log("Inserting " + amountOfNewLines + " line(s) after line: " + (contentChange.range.start.line + 1));
+            this.bracketsPerLine =
+                this.bracketsPerLine.slice(0, contentChange.range.start.line)
+                    .concat(emptyBrackets)
+                    .concat(this.bracketsPerLine
+                        .slice(contentChange.range.start.line));
+
+            // lowestLineNumberChanged = Math.min(lowestLineNumberChanged, contentChange.range.start.line);
+            // highestLineNumberChanged = Math.max(highestLineNumberChanged, contentChange.range.start.line + amountOfNewLines);
+
+        } // Line(s) deleted
+        else if (contentChange.text === "" && contentChange.range.start.line !== contentChange.range.end.line) {
+            console.log("Removing lines between: " + (contentChange.range.start.line + 1) + ", " + (contentChange.range.end.line + 1));
+            let linesToBeRemoved = contentChange.range.end.line - contentChange.range.start.line;
+            this.bracketsPerLine.splice(contentChange.range.start.line, linesToBeRemoved);
+        }
+        // Text deleted
+        else if (contentChange.text === "") {
+            console.log("Text deleted at line : " + (contentChange.range.start.line + 1));
+        }
+        // Text contains a bracket
+        else if (regex.test(contentChange.text)) {
+            console.log("Text added at line : " + (contentChange.range.start.line + 1));
+        }
+        // No need to update
+        else {
+            return false;
+        }
+
+        return true;
+    }
+
+    updateDecorations(lineNumber: number = 0) {
+
+        console.log("Colorizing brackets from line: " + (lineNumber + 1));
 
         // TODO Move errorBracket to brackets array and count amount of unmatched brackets
         let errorBracket: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({
@@ -70,22 +120,20 @@ export default class Document {
             text = this.textEditor.document.getText();
         }
 
-        let bracketCount: { [character: string]: number; } = {};
+        let bracketCount = this.getEmptyBracketArray();
         let decorations = new Map<vscode.TextEditorDecorationType, vscode.Range[]>();
-
-        for (let bracketPair of this.bracketPairs) {
-            bracketCount[bracketPair.openCharacter] = 0;
-            bracketCount[bracketPair.closeCharacter] = 0;
-        }
 
         let regex = new RegExp(this.regexPattern, "g");
 
         let match: RegExpExecArray | null;
         let previousLineNumber = -1;
         while ((match = regex.exec(text)) !== null) {
-            let startPos = new vscode.Position(this.textEditor.document.positionAt(match.index).line + lineNumber, this.textEditor.document.positionAt(match.index).character);
+            let textPos = this.textEditor.document.positionAt(match.index);
+            let startPos = new vscode.Position(textPos.line + lineNumber, textPos.character);
             let endPos = startPos.translate(0, match[0].length);
             let range = new vscode.Range(startPos, endPos);
+
+            //console.log("Found bracket at Line: " + (startPos.line + 1) + ", Character: " + startPos.character);
 
             for (let bracketPair of this.bracketPairs) {
                 // If open bracket matches, store the position and color, increment count 
